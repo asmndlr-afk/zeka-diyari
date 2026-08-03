6b069db89bac3af7f67dc81180199b3bf2f93f48
asmndlr-afk <asmndlr-afk@users.noreply.github.com>
2026-08-03 15:04:17 +0300

    perf: Optimize pixar_heads and pixar_tiles images for instant loading in memory cards

--- resize.ps1
+++ resize.ps1
@@ -1,16 +1,19 @@
 Add-Type -AssemblyName System.Drawing
-$images = Get-ChildItem -Path "assets\images\avatars" -Include *.png, *.jpg -Recurse
-foreach ($img in $images) {
-    try {
-        $bmp = New-Object System.Drawing.Bitmap($img.FullName)
-        $newBmp = New-Object System.Drawing.Bitmap($bmp, 150, 150)
-        $bmp.Dispose()
-        $imgName = $img.FullName
-        Remove-Item -Path $imgName -Force
-        $newBmp.Save($imgName, [System.Drawing.Imaging.ImageFormat]::Jpeg)
-        $newBmp.Dispose()
-        Write-Output ("Resized: " + $imgName)
-    } catch {
-        Write-Output ("Failed: " + $img.FullName)
+$folders = @('assets\images\pixar_heads', 'assets\images\pixar_fruits', 'assets\images\pixar_tiles')
+foreach ($folder in $folders) {
+    $images = Get-ChildItem -Path $folder -Include *.png, *.jpg -Recurse
+    foreach ($img in $images) {
+        try {
+            $bmp = New-Object System.Drawing.Bitmap($img.FullName)
+            $newBmp = New-Object System.Drawing.Bitmap($bmp, 150, 150)
+            $bmp.Dispose()
+            $imgName = $img.FullName
+            Remove-Item -Path $imgName -Force
+            $newBmp.Save($imgName, [System.Drawing.Imaging.ImageFormat]::Jpeg)
+            $newBmp.Dispose()
+            Write-Output ('Resized: ' + $imgName)
+        } catch {
+            Write-Output ('Failed: ' + $img.FullName)
+        }
     }
 }
